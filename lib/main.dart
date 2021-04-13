574fc20e067e7eac063267d04385f97b6fc63ed4
Tryo Asnafi <tryoasnafi@gmail.com>
2021-04-13 12:15:14 +0700

    Media Query

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController(text: "Nilai Awal");
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan TextField Wiget"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextField(
-                // obscureText: true,
-                // maxLines: 3,
-                maxLength: 10,
-                controller: controller,
-                onChanged: (value) {
-                  setState(() {});
-                },
-              ),
-              Text(controller.text),
-            ],
-          ),
-        ),
-      ),
+      home: MainPage(),
     );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Media Query"),
+      ),
+      body: (MediaQuery.of(context).orientation == Orientation.portrait)
+          ? Column(
+              children: generateContainers(),
+            )
+          : Row(
+              children: generateContainers(),
+            ),
+    );
+  }
+
+  List<Widget> generateContainers() {
+    return [
+      Container(
+        color: Colors.amber,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.green,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100,
+      ),
+    ];
   }
 }
