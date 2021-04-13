aaa0e2d2785a61be07876f4caedbb62c6420bca0
Tryo Asnafi <tryoasnafi@gmail.com>
2021-04-13 11:55:48 +0700

    TextField Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,40 @@
-import 'package:first_flutter_app/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller = TextEditingController(text: "Nilai Awal");
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan TextField Wiget"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextField(
+                // obscureText: true,
+                // maxLines: 3,
+                maxLength: 10,
+                controller: controller,
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+              Text(controller.text),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
